55b5e3b8a7bec99c005fdcdac9e2e59ae874c699
antoan <antoan@antoan.com>
2018-09-14 18:35:42 +0100

    changed theme, unselected cyber

--- init.el
+++ init.el
@@ -8,6 +8,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (doom-one)))
  '(org-agenda-files (quote ("/media/tony/06E5-C286/org/plan.org")))
  '(package-selected-packages (quote (doom-themes cyberpunk-theme))))
 (custom-set-faces
@@ -18,7 +19,30 @@
  )
 
 
-(load-theme 'cyberpunk t)
+;; (load-theme 'cyberpunk t)
+
+(require 'doom-themes)
+
+;; Global settings (defaults)
+(setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+      doom-themes-enable-italic t) ; if nil, italics is universally disabled
+
+;; Load the theme (doom-one, doom-molokai, etc); keep in mind that each theme
+;; may have their own settings.
+(load-theme 'doom-one t)
+
+;; Enable flashing mode-line on errors
+(doom-themes-visual-bell-config)
+
+
+;; Enable custom neotree theme (all-the-icons must be installed!)
+(doom-themes-neotree-config)
+;; or for treemacs users
+(doom-themes-treemacs-config)
+
+;; Corrects (and improves) org-mode's native fontification.
+(doom-themes-org-config)
+
 (setq org-refile-targets '((org-agenda-files . (:maxlevel . 6))))
 (setq org-outline-path-complete-in-steps nil)         ; Refile in a single go
 (setq org-refile-use-outline-path t)                  ; Show full paths for refiling
