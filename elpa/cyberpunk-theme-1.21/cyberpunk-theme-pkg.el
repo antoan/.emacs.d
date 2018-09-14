6c874665257e54407823f3ed6ce57de305969153
antoan <antoan@antoan.com>
2018-09-14 11:34:21 +0100

    base

---
+++ elpa/cyberpunk-theme-1.21/cyberpunk-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "cyberpunk-theme" "1.21" "Cyberpunk Color Theme" 'nil)
---
+++ init.el
@@ -0,0 +1,26 @@
+;; This sets up the load path so that we can override it
+(package-initialize)
+
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files (quote ("/media/tony/06E5-C286/org/plan.org")))
+ '(package-selected-packages (quote (cyberpunk-theme))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(load-theme 'cyberpunk t)
+(setq org-refile-targets '((org-agenda-files . (:maxlevel . 6))))
+(setq org-outline-path-complete-in-steps nil)         ; Refile in a single go
+(setq org-refile-use-outline-path t)                  ; Show full paths for refiling
+
+(ido-mode)
+(setq org-completion-use-ido t)
