f7f5a8259b9a3ecc2f78840d76888951ced6da3a
antoan <antoan.bekele@gmail.com>
2018-09-17 10:58:49 +0100

    helm mini kbd

--- init.el
+++ init.el
@@ -72,6 +72,7 @@
 
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
 (global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "C-x b") 'helm-mini)
 
 (when (executable-find "curl")
   (setq helm-google-suggest-use-curl-p t))
